2f4b07fb38bc80d5be582a0a4c028c578d92a496
Andrei Baechle <43485840+andreiBaechle@users.noreply.github.com>
2018-09-22 02:22:59 -0500

    Add files via upload

---
+++ BasketBall/Basketballtest.java
@@ -0,0 +1,22 @@
+package lab2;
+
+public class Basketballtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Basketball b;
+	b = new Basketball(4.0);
+	
+	System.out.println(b.getDiameter());
+	System.out.println(b.isDribbleable());
+	
+	Basketball b2 = new Basketball(6.0);
+	
+	b.inflate();
+	System.out.println(b.isDribbleable());  // should be true now
+	System.out.println(b2.isDribbleable()); // should still be false
+
+	}
+
+}
